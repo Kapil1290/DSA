f8f548c20ac78f524b6c831fd10dc2dad8e7bef7
Kapil1290 <kapilprajapat9303@gmail.com>
2026-01-13 18:08:48 +0530

    addCode

--- Code1/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-    int a, b;
-    cin>>a>>b;
-    int c=a+b;
-    cout<<"Sum is: " << c << endl;    return 0;
-}
---
+++ Lecture_4/1_addTwoNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter 1st Num:- ";
+    cin>>a;
+    cout<<"\nEnter 2nd Num:- ";
+    cin>>b;
+
+    int c=a+b;
+    cout<<"\nSum is:-"<<c;
+}
