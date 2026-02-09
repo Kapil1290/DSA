f263d8bc26c3f3859e64f1d212347f7876cacad2
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-09 14:14:57 +0530

    Operation

--- Lecture_8/Basic.cpp
+++ Lecture_8/Basic.cpp
@@ -5,11 +5,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[5] = {10,20,30,40,50};
+    int x[3];
+    
+    int a[5] = {1,1,1,1,1};
     int size = sizeof(a)/sizeof(int);
     cout<<"Size is: "<<size<<endl;;
     for(int i=0; i<size; i++){
-        cout<<a[i]<<endl;;
+        cout<<a[i];
+    }
+    cout<<endl;
+    for(int i=0; i<size; i++){
+        cout<<a[i]+i;
     }
     
 }
