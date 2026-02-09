831641ade234cf7a0ad042f7abf21624c3539bb7
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-09 15:46:07 +0530

    Sum of array elements

--- LeetCode/2_max.cpp
+++ LeetCode/2_max.cpp
@@ -1,14 +1,33 @@
-#include<iostream>
-using namespace std;
-int main(){
 
-    int arr[] = {22,5,6,2,5};
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int arr[] = {22,-5,6,2,5};
     int max = 0;
-    int size = sizeof(arr)/sizeof(int);
+    int size = sizeof(arr)/sizeof(int);  // return # of values in array
     for(int i=0; i<size-1; i++){
         if(arr[max]<arr[max+1]){
             max++;
         }
     }
-    cout<<arr[max];
+    cout<<"Max. val is: "<<arr[max]<<endl;;
+
+    int min = 0;
+    for(int i=0; i<size-1; i++){
+        if(arr[min]>arr[min+1]){
+            min++;
+        }
+    }
+    cout<<"Min. val is: "<<arr[min]<<endl;;
+
+    int sum = 0;
+    for(int i=0; i<size; i++){
+        sum = sum + arr[i];
+        cout<<arr[i];
+    }
+    cout<<"Sum is: "<<sum;
+
+    return 0;
 }
---
+++ LeetCode/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    int sum = 0;
+    for(int i=0; i<size; i++){
+        sum = sum + arr[i];
+    }
+    cout<<"Sum is: "<<sum;
