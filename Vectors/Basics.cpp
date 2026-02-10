88eed95d5e2e53623086f32fd03bcd1a952e7b90
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-10 15:55:10 +0530

    arrays in vector

--- Vectors/Basics.cpp
+++ Vectors/Basics.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main(){
     vector<int> a;
-    int val;
-    for(int i=1; i<=5; i++){
+    int val,cap;
+    cout<<"Give size of array:- ";
+    cin>>cap;
+    for(int i=1; i<=cap; i++){
         cin>>val;
         a.push_back(val);
     }
-    cout<<a.size()<<endl;;
+
+    cout<<a.size()<<endl; // for size
+    cout<<a.capacity()<<endl; // for capacity
+
+    // printing of elements
     for(int i=0; i<5; i++){
         cout<<a.at(i)<<" ";
     }
-
 }
+    
