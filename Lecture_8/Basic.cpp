13cb322004902a21759b6ea8172e8deee2a9f6c2
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-11 19:08:13 +0530

    problem solving

--- Lecture_8/Basic.cpp
+++ Lecture_8/Basic.cpp
@@ -2,20 +2,62 @@
 // # Where each element have its index
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // int x[3];
+    
+//     // int a[5] = {1,1,1,1,1};
+//     // int size = sizeof(a)/sizeof(int);
+//     // cout<<"Size is: "<<size<<endl;;
+//     // for(int i=0; i<size; i++){
+//     //     cout<<a[i];
+//     // }
+//     // cout<<endl;
+//     // for(int i=0; i<size; i++){
+//     //     cout<<a[i]+i;
+//     // }
+    
+
+// }
+
+
 #include<iostream>
 using namespace std;
-int main(){
-    int x[3];
-    
-    int a[5] = {1,1,1,1,1};
-    int size = sizeof(a)/sizeof(int);
-    cout<<"Size is: "<<size<<endl;;
-    for(int i=0; i<size; i++){
-        cout<<a[i];
-    }
-    cout<<endl;
-    for(int i=0; i<size; i++){
-        cout<<a[i]+i;
-    }
-    
+
+int main() {
+	long long int n;
+	cin>>n;
+	long long int copyOfN = n;
+	int count=0;
+	int count1;
+
+    if(n==0){
+		cout<<"5";
+	}else{
+		// for number of digits of value
+	while(copyOfN!=0){
+		copyOfN=copyOfN / 10;
+		count++;
+	}
+	count1=count;
+	int arr[count1];
+	int i=0;
+	while(count>0){
+		int rem = n%10;
+		arr[i] = rem;
+		n=n/10;
+		i++; count--;
+	}
+
+	for(int i=0; i<count1; i++){
+		if(arr[i]==0){
+			arr[i]=5;
+		}
+	}
+
+	for(int i=count1-1; i>=0; i--){
+		cout<<arr[i];
+	}
+	}
 }
