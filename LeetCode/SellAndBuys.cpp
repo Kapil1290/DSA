74c51c5268f27b2c25ae8121d43058bb66a5f9d5
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-12 20:44:56 +0530

    problem solving

--- LeetCode/SellAndBuys.cpp
+++ LeetCode/SellAndBuys.cpp
@@ -2,17 +2,29 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> a;
-    a.push_back(1);
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(2);
-    a.push_back(3);
-
-    vector<int> b;
-    b.push_back(a.at(0));
-    int i=0;
-    
-
+    vector<int> prices;
+    prices.push_back(7);
+    prices.push_back(1);
+    prices.push_back(5);
+    prices.push_back(3);
+    prices.push_back(6);
+    prices.push_back(4);
+    int minPrice = 0;
+        for(int i=0; i<prices.size(); i++){
+            if(prices.at(i)<prices.at(minPrice)){
+                minPrice = i;
+            }
+        }
+        // cout<<minPrice<<endl;
+        int maxPrice;
+        int i=minPrice;
+        while(i<prices.size()-1){
+            if(prices.at(i)<prices.at(i+1)){
+                maxPrice = i+1;
+            }
+            i++;
+        }
+        // cout<<maxPrice<<endl;
+    cout<<prices.at(maxPrice)-prices.at(minPrice);    
 
 }
