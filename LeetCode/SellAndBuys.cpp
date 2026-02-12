240459d796f0a06fa84185ed61e7d1ca6ad02379
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-12 16:15:09 +0530

    problem solving

---
+++ LeetCode/SellAndBuys.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(2);
+    a.push_back(3);
+
+    vector<int> b;
+    b.push_back(a.at(0));
+    int i=0;
+    
+
+
+}
