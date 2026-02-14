3c5feee519cf69cda5e22f5f556758ea1d9d53ce
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-14 12:09:47 +0530

    2d array understanding

--- Lecture_8/temp.cpp
+++ Lecture_8/temp.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main(){
 	int arr[2][3] = {{2,1,2},{2,34,3}};
-	
+	int row = sizeof(arr)/sizeof(arr[0]);
+	int col = sizeof(arr[0])/sizeof(arr[0][0]);
+	cout<<sizeof(arr)<<endl;
+	cout<<row<<endl;
+	cout<<col<<endl;
 	string s = "hello";
 	cout<<s.at(0);
 	return 0;
