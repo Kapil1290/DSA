e2e7f6a5fb15df1a450b56d8281d39f31bbd7cd0
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-14 11:50:58 +0530

    leetcode problem solving

--- Lecture_8/temp.cpp
+++ Lecture_8/temp.cpp
@@ -1,37 +1,10 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
-
-	int cas;
-	cin>>cas;
-	int sum=0;
-	long long arr[cas];
-	for(int i=0; i<cas; i++){
-		cin>>arr[i];
-	}
-	for(int i=0; i<cas; i++){
-		sum=0;
-		long long temp = arr[i];
-		while(temp!=0){
-			int rem = temp%10;
-			sum = sum + rem;
-			temp=temp/10;
-		}
-		if(sum%2==0){
-			if(sum%4==0){
-				cout<<"Yes"<<endl;
-			}else{
-				cout<<"No"<<endl;
-			}
-		}else{
-			if(sum%3==0){
-				cout<<"Yes"<<endl;
-			}else{
-				cout<<"No"<<endl;
-			}
-		}
-	}
-
+	
+	string s = "hello";
+	cout<<s.at(0);
 	return 0;
 }
