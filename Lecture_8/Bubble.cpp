22f89fe4e2f771c58025acffaa60b8cf5313a74a
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-18 14:23:38 +0530

    sorting in cpp

--- Lecture_8/Bubble.cpp
+++ Lecture_8/Bubble.cpp
@@ -2,15 +2,16 @@
 #include<vector>
 using namespace std;
 int main(){
+    
     int arr[] = {2,5,8,9,1};
     int size = sizeof(arr)/sizeof(int);
-    cout<<size;
+
     for(int i=0; i<size-1; i++){
-        for(int j=0; j<size-i-1; j++){
+        for(int j=0; j<size-1-i; j++){
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
-                arr[i] = temp;
+                arr[j+1] = temp;
             }
         }
     } 
