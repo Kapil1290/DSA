5dd8d72920013c0400cbdfae17f574f48f6ea08a
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-18 14:37:14 +0530

    selection sort in cpp

---
+++ Lecture_8/SelectionSort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    
+    int arr[] = {2,5,8,9,1};
+    int size = sizeof(arr)/sizeof(int);
+
+    for(int i=0; i<size; i++){
+        int min = i;
+        for(int j=i+1; j<size; j++){
+            if(arr[min]<arr[i]){
+                min = j;
+            }
+        }
+    }
+
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
