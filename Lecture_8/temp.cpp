555a3c8b991548cd2156e557fe470df72226bee5
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-19 21:07:24 +0530

    leetcode problem done

--- Lecture_8/temp.cpp
+++ Lecture_8/temp.cpp
@@ -8,11 +8,11 @@ int main() {
 	int val = 3;
 	int size = sizeof(nums)/sizeof(int);
     int count=0;
-        for(int i=1; i<size; i++){
-            if(nums[i]==val){
-                count++;
-                nums[count]=nums[i];
-            }
+        for(int i=0; i<size; i++){
+            if(nums[i]!=val){
+				nums[count]=nums[i];
+				count++;
+			}
         }
         cout<<count;
 }
