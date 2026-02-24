ba76d0371f4dc405b7db31e53cede265360b809a
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-24 13:22:24 +0530

    leetcode problem

--- LeetCode/medianOfTwoSortArray.cpp
+++ LeetCode/medianOfTwoSortArray.cpp
@@ -1,19 +1,35 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<math>
 using namespace std;
 int main(){
     vector<int> a;
     vector<int> b;
-    a.push_back(1);
-    a.push_back(2);
-    b.push_back(3);
-    b.push_back(4);
+    a.push_back(3);
+    a.push_back(4);
+    b.push_back(2);
+    b.push_back(7);
 
     for(int i=0; i<b.size(); i++){
         a.push_back(b[i]);
     }
 
+    sort(a.begin(),a.end());
     for(int i=0; i<a.size(); i++){
         cout<<a.at(i)<<endl;
     }
+
+    if(a.size()%2==0){
+        int idx = a.size()/2;
+        double res = (a[idx]+a[idx-1])/2;
+        return res;
+    }else{
+        int idx = ceil(a.size()/2);
+        double res = a[idx];
+        return res;
+    }
+
+
+    
 }
