0a50484136599561b6a50e46794909666bd1d0eb
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-24 17:35:35 +0530

    monotone problem done

--- LeetCode/medianOfTwoSortArray.cpp
+++ LeetCode/medianOfTwoSortArray.cpp
@@ -34,8 +34,8 @@ vector<int> MergeSort(vector<int> a, vector<int> b){
 int main(){
     vector<int> a;
     vector<int> b;
-    a.push_back(1);
-    a.push_back(2);
+    a.push_back(3);
+    a.push_back(5);
     b.push_back(3);
     b.push_back(4);
 
