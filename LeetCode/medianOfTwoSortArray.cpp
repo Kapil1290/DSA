13e74245f5d23dea647e41931fd502ab883d6d1c
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-24 16:55:39 +0530

    dsa solving

--- LeetCode/medianOfTwoSortArray.cpp
+++ LeetCode/medianOfTwoSortArray.cpp
@@ -3,6 +3,10 @@
 #include<algorithm>
 #include<cmath>
 using namespace std;
+
+int MergeSort(vector<int> a, vector<int> b){
+    
+}
 int main(){
     vector<int> a;
     vector<int> b;
@@ -11,24 +15,24 @@ int main(){
     b.push_back(3);
     b.push_back(4);
 
-    for(int i=0; i<b.size(); i++){
-        a.push_back(b[i]);
-    }
+    // for(int i=0; i<b.size(); i++){
+    //     a.push_back(b[i]);
+    // }
 
-    sort(a.begin(),a.end());
-    for(int i=0; i<a.size(); i++){
-        cout<<a.at(i)<<endl;
-    }
+    // sort(a.begin(),a.end());
+    // for(int i=0; i<a.size(); i++){
+    //     cout<<a.at(i)<<endl;
+    // }
 
-    if(a.size()%2==0){
-        int idx = a.size()/2.00;
-        double res = (a[idx]+a[idx-1])/2.0;
-        cout<<res; 
-    }else{
-        int idx = ceil(a.size()/2.0);
-        double res = a[idx];
-        cout<<res;
-    }
+    // if(a.size()%2==0){
+    //     int idx = a.size()/2.00;
+    //     double res = (a[idx]+a[idx-1])/2.0;
+    //     cout<<res; 
+    // }else{
+    //     int idx = ceil(a.size()/2.0);
+    //     double res = a[idx];
+    //     cout<<res;
+    // }
 
     // for(int i=0; i<a.size(); i++){
     //     cout<<a.at(i)<<endl;
