8b80e4d8fbcacc7a9a5c35d690321c46f5c3b5fb
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-24 17:02:34 +0530

    merge sort

--- LeetCode/medianOfTwoSortArray.cpp
+++ LeetCode/medianOfTwoSortArray.cpp
@@ -4,8 +4,32 @@
 #include<cmath>
 using namespace std;
 
-int MergeSort(vector<int> a, vector<int> b){
+vector<int> MergeSort(vector<int> a, vector<int> b){
     
+    vector<int> merge;
+    int i=0;
+    int j=0;
+    while(i<a.size() && j<b.size()){
+        if(a[i]<b[j]){
+            merge.push_back(a[i]);
+            i++;
+        }else{
+            merge.push_back(b[j]);
+            j++;
+        }
+    }
+    while(i<a.size()){
+        merge.push_back(a[i]);
+        i++;
+    }
+
+    while(j<b.size()){
+        merge.push_back(b[j]);
+        j++;
+    }
+
+    return merge;
+
 }
 int main(){
     vector<int> a;
@@ -38,5 +62,8 @@ int main(){
     //     cout<<a.at(i)<<endl;
     // }
 
-    
+    vector<int> m = MergeSort(a,b);
+    for(int i=0; i<m.size(); i++){
+        cout<<m[i]<<" ";
+    }
 }
