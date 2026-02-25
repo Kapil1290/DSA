2ea5ada5b2a3c82c367fed4d9a5b6bea2399910b
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-25 14:33:37 +0530

    2d problem

--- 2D Array/Basic.cpp
+++ 2D Array/Basic.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main() {
 
@@ -6,5 +7,11 @@ int main() {
     {1,2,3,4},{12,12,3,4},{45,33,66,7},{1,21,22,13}
     };
 
-    
+    int size1 = sizeof(arr)/(sizeof(int)*sizeof(int));
+    cout<<size1;
+    int arr1[2][3] = {1,2,3,4,5,6};
+
+    // for(int i=0; i<arr.size(); i++){
+    //     for(int j=0; j<arr[0].)
+    // }
 }
