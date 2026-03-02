8796abdada54b1d56de737d90142645ade8d0bcb
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-02 09:43:06 +0530

    returing a element of pascal tree using ncr method

--- 2D Array/Pascal-1.cpp
+++ 2D Array/Pascal-1.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int row = 5, col = 3;
+    int n = 5, r = 3;
     int res=1;
-    for(int i=0; i<row; i++){
-        res = res*()
+    for(int i=0; i<r; i++){
+        res = res*(n-i);
+        res = res/i+1;
     }
+    cout<<res;
 }
