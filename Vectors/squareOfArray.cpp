300b62a6df52276f6bcd612071f1e561a2b90af1
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-03 09:51:12 +0530

    problem solving

---
+++ Vectors/squareOfArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> a;
+    a.push_back(-4);
+    a.push_back(-1);
+    a.push_back(0);
+    a.push_back(3);
+    a.push_back(2);
+
+    sort(a.begin(),a.end());
+
+}
