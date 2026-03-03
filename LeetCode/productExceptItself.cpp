ceae44fceab5a21921becb0828fb1ad2130b81f7
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-03 15:48:33 +0530

    finding the approach

--- LeetCode/productExceptItself.cpp
+++ LeetCode/productExceptItself.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> prefix;
-    vector<int> suffix;
+    int prefix[n];
+    int suffix[n];
     vector<int> a;
     int temp;
     for(int i=0; i<n; i++){
         cin>>temp;
         a.push_back(temp);
     }
-
+    int m = a.size()-1;
     // for prefix---
-    prefix.push_back(1);
-    suffix[a.size()-1] = 1;
-    cout<<suffix[a.size()-1];
+    prefix[0] = 1;
+    suffix[m] = 1;
+
+    
     return 0;
 }
