71fa5d5613dc42566e9f8fe7d7251a1dfb43103f
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-03 10:01:52 +0530

    square array problem done

--- Vectors/squareOfArray.cpp
+++ Vectors/squareOfArray.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 using namespace std;
 
 int main(){
@@ -9,9 +10,19 @@ int main(){
     a.push_back(-1);
     a.push_back(0);
     a.push_back(3);
-    a.push_back(2);
+    a.push_back(10);
 
+    for(int i=0; i<a.size(); i++){
+        int temp = abs(a[i]);
+        a[i] = temp;
+    }
+    
     sort(a.begin(),a.end());
+    
+    for(int i=0; i<a.size(); i++){
+        int temp = a[i]*a[i];
+        a[i] = temp;
+    }
 
     for(int i=0; i<a.size(); i++){
         cout<<a[i]<<" ";
