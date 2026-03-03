c018355e6e6437c48f575d3cd927dc53554ef416
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-03 10:27:44 +0530

    solving new problem

---
+++ Vectors/cyclicArr.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	vector<int> a;
+	int n; 
+	cin>>n;
+	int temp;
+	for(int i=0; i<n; i++){
+		cin>>temp;
+		a.push_back(temp);
+	}
+	int n1;
+	cin>>n1;
+	vector<int> a1;
+	for(int i=0; i<n1; i++){
+		cin>>temp;
+		a1.push_back(temp);
+	}
+
+	for(int i=0; i<n1; i++){
+		int t1=a1[i];
+		if(t1!=0){
+			for(int i=0; i<a.size(); i++){
+				if(i==0){
+					a[i] = a[i]+a[a.size()-1];
+				}else{
+					a[i] = a[i]+a[i-1];
+				}
+			}
+		}else{
+			for(int i=0; i<a.size(); i++){
+				a[i] = a[i] + a[i];
+			}
+		}
+	}
+
+	for(int i=0; i<a.size(); i++){
+		cout<<a[i]<<" ";
+	}
+
+
+}	
