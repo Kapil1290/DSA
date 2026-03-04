dd1057265ed5df331a109bd90de7885b0f7ea580
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-04 08:12:24 +0530

    changing the arrray elements

--- Vectors/cyclicArr.cpp
+++ Vectors/cyclicArr.cpp
@@ -22,10 +22,8 @@ int main() {
     vector<int> a;
     a.push_back(1);
     a.push_back(2);
-    a.push_back(3);
-	a.push_back(4);
-	a.push_back(5);
-	
+	a.push_back(1);
+
     int n1=2;
     vector<int> a1;
     a1.push_back(1);
