b8bd806dcc70a5f3e5bc371c5f2717c65128af45
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-05 10:30:44 +0530

    Problem solving but not solved yet

--- Vectors/cyclicArr.cpp
+++ Vectors/cyclicArr.cpp
@@ -3,98 +3,53 @@
 #include<vector>
 using namespace std;
 int main() {
-	// vector<int> a;
-	// int n; 
-	// cin>>n;
-	// int temp;
-	// for(int i=0; i<n; i++){
-	// 	cin>>temp;
-	// 	a.push_back(temp);
-	// }
-	// int n1;
-	// cin>>n1;
-	// vector<int> a1;
-	// for(int i=0; i<n1; i++){
-	// 	cin>>temp;
-	// 	a1.push_back(temp);
-	// }
-    // int n=5;
-    // vector<int> a;
-    // a.push_back(1);
-    // a.push_back(2);
-	// a.push_back(1);
-
-    // int n1=2;
-    // vector<int> a1;
-    // a1.push_back(1);
-    // a1.push_back(0);
-
-    // vector<int> newarray;
-
-	// for(int i=0; i<a1.size(); i++){
-	// 	int t1=a1[i];
-	// 	if(t1!=0){
-	// 		for(int i=0; i<a.size(); i++){
-    //             int x;
-	// 			if(i==0){
-    //                 int end=a.size()-1;
-	// 				x = a[i]+a[end];
-    //                 newarray.push_back(x);
-	// 			}else{
-	// 				x = a[i]+a[i-1];
-    //                 newarray.push_back(x);
-	// 			}
-	// 			cout<<newarray[i]<<" ";
-	// 		}
-	// 	}else{
-	// 		for(int i=0; i<a.size(); i++){
-	// 			int t = newarray[i] + newarray[i];
-    //             newarray[i] = t;
-	// 			cout<<newarray[i]<<" ";
-	// 		}
-	// 	}
-	// }
-    // cout<<endl;
-    // int sum=0;
-	// for(int i=0; i<newarray.size(); i++){
-	// 	sum += newarray[i];
-	// }
-    // int val = pow(10,9)+7;
-    // cout<<sum%val;
-
-
-	// for(int i=0; i<n; i++){
-	// 	int a = (n+i)%n;
-	// }
-	int flag=0;
 	int n; 
 	cin>>n;
 	int a[n];
 	for(int i=0; i<n; i++){
 		cin>>a[i];
 	}
-	int x=n+1;
-	int b[x];
-	for(int i=0; i<n; i++){
-		b[i+1] = a[i];
+	int checktest;
+	cin>>checktest;
+	int check[checktest];
+	for(int i=0; i<checktest; i++){
+		cin>>check[i];
 	}
-	b[0] = a[n-1];
-	int check[] = {1,0};
-	int j=1;
-	int one = 1;
+
+	int ones=0,zeroes=0;
+	for(int i=0; i<checktest; i++){
+		if(check[i]==0){
+			zeroes++;
+		}else{
+			ones++;
+		}
+	}
+
 	int temp;
-	for(int i=0; i<n && j<=one;  i++){
+	int j=1;
+	for(int i=0; i<=n && j<=ones;  i++){
 		if(i==0){
 			temp = a[i];
 			a[i] = a[n-1]+temp;
 
-		}else if(i>0){
+		}else if(i==n){
+			i=-1; j++;
+			continue;
+		}else{
+			int x = temp;
 			temp = a[i];
-			a[i] = temp + a[i]
+			a[i] = x + a[i];
+			
 		}
+		
 	}
-
-	for(int i=0; i<x-1; i++){
-		cout<<a[i]<<" ";
+	int sum=0;
+	for(int i=0; i<n; i++){
+		sum += a[i];
 	}
+	for(int i=1; i<=zeroes; i++){
+		sum *= 2;
+	}
+	int val = pow(10,9)+7;
+	cout<<sum%val;
 }	
