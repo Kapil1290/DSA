0b8a969e546215cb088b80315d975745564a9714
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-10 16:33:40 +0530

    doing new question of the leetcode

--- 2D Array/DiagonalMatrix.cpp
+++ 2D Array/DiagonalMatrix.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int arr[][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+    int m = arr.size();
+    int n = arr[0].size();
 }
