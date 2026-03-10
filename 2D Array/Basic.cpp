f5a67148a5bc3858dafd33590d467f1def1688df
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-10 17:19:24 +0530

    finding row and colums of the matrix;

--- 2D Array/Basic.cpp
+++ 2D Array/Basic.cpp
@@ -7,9 +7,13 @@ int main() {
     {1,2,3,4},{12,12,3,4},{45,33,66,7},{1,21,22,13},{14,15,16,17}
     };
 
-    int size = sizeof(arr);
+    int size = sizeof(arr)/sizeof(int);
+    int row = sizeof(arr)/(sizeof(int)*sizeof(int));
+    cout<<row<<endl;
+    int col = sizeof(arr[0])/sizeof(int);
+    cout<<col<<endl;
     // int size = sizeof(arr);
-    cout<<size;
+    // cout<<size;
     int arr1[2][3] = {1,2,3,4,5,6};
 
     // for(int i=0; i<arr.size(); i++){
