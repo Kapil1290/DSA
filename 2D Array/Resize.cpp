27660b623821ce1665ddcd55753da7c3c9fdc4f3
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-11 17:52:59 +0530

    finding approach

--- 2D Array/Resize.cpp
+++ 2D Array/Resize.cpp
@@ -8,6 +8,8 @@ int main() {
         {3,4,2}
     };
 
+    int size = sizeof(mat);
+    cout<<size<<endl;
     int r = sizeof(mat)/(sizeof(int)*sizeof(int));
     int c = sizeof(mat[0])/sizeof(int);
     cout<<r<<" "<<c;
