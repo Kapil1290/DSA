780befe9b5924c6f3514d499e15393b9b2cf5b97
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-11 21:54:40 +0530

    problem solving

--- 2D Array/Resize.cpp
+++ 2D Array/Resize.cpp
@@ -8,10 +8,8 @@ int main() {
         {3,4,2}
     };
 
-    int size = sizeof(mat);
-    cout<<size<<endl;
-    int r = sizeof(mat)/(sizeof(int)*sizeof(int));
+    int r = sizeof(mat)/sizeof(mat[0]);
     int c = sizeof(mat[0])/sizeof(int);
     cout<<r<<" "<<c;
-    // int arr[mat.]
+    
 }
