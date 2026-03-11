e201e9227f6ee5e55c8d7a6cc368c764a73781da
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-11 17:42:26 +0530

    problem solving

--- 2D Array/Resize.cpp
+++ 2D Array/Resize.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int main() {
-    int mat[][2] = {
-        {1,2},
-        {3,4}
+    int mat[][3] = {
+        {1,2,5},
+        {3,4,2}
     };
 
-    
+    int r = sizeof(mat)/(sizeof(int)*sizeof(int));
+    int c = sizeof(mat[0])/sizeof(int);
+    cout<<r<<" "<<c;
+    // int arr[mat.]
 }
