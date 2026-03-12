d06b4336a79c7f9daf62b8aa7ba760ad1cd6a725
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-12 11:40:02 +0530

    new approach

--- 2D Array/setZeros.cpp
+++ 2D Array/setZeros.cpp
@@ -11,16 +11,18 @@ int main(){
     int n = sizeof(matrix[0])/sizeof(int);
 
      for(int i=0; i<m; i++){
-        int t1 = 0;
-                int t2 = 0;
+        
             for(int j=0; j<n; j++){
-                
+                int t1 = 0;
+                int t2 = 0;
                 if(matrix[i][j]==0){
                     while(t1<m && t2<n){
-                        matrix[t1][j] = 0;
-                        matrix[i][t2] = 0;
-                        t1++;
-                        t2++;
+                        if(t1!=i || t2!=j){
+                            matrix[t1][i] = -1;
+                            matrix[j][t2] = -1;
+                            t1++;
+                            t2++;
+                        }
                     }
                     
                 }
