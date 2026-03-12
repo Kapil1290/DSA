332c2589e5216e0a3e26343dbf7d6a1227de9c84
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-12 08:36:20 +0530

    problem solving

--- 2D Array/Resize.cpp
+++ 2D Array/Resize.cpp
@@ -3,13 +3,37 @@
 using namespace std;
 
 int main() {
-    int mat[][3] = {
-        {1,2,5},
-        {3,4,2}
+    int mat[][2] = {
+        {1,5},
+        {3,2}
     };
 
+    int size = sizeof(mat)/sizeof(int);
     int r = sizeof(mat)/sizeof(mat[0]);
     int c = sizeof(mat[0])/sizeof(int);
-    cout<<r<<" "<<c;
+    // cout<<size;
+    int row = 1,col = 4;
+    int res[row][col]; 
     
+    int arr[row*col];
+    int k=0;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            arr[k++] = mat[i][j];
+        }
+    }
+    k=0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            res[i][j] = arr[k++];
+        }
+    }
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout<<res[i][j]<<" ";
+        }
+    }
+
+
 }
