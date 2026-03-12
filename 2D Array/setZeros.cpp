33e091c98ca32927b2cb4be7c0b181e052cacc4f
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-12 11:20:05 +0530

    approach mis matching

--- 2D Array/setZeros.cpp
+++ 2D Array/setZeros.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int matrix[][3] = {
+        {1,1,1},
+        {1,0,1},
+        {1,1,1}
+    };
+
+    int m = sizeof(matrix)/sizeof(matrix[0]);
+    int n = sizeof(matrix[0])/sizeof(int);
+
+     for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int t1 = 0;
+                int t2 = 0;
+                if(matrix[i][j]==0){
+                    
+
+                    while(t1<m && t2<n){
+                        matrix[t1][j] = 0;
+                        matrix[i][t2] = 0;
+                        t1++;
+                        t2++;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
     
-}
+}    
+    
