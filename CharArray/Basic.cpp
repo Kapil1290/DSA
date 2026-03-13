517e7684e31643f8535d84c4c2d9b5d4d4072ce2
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-13 14:18:59 +0530

    new topic started

---
+++ CharArray/Basic.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    char arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+    char a[] = {'A','B','C','D'};
+    int size = sizeof(a)/sizeof(char);
+    for(int i=0; i<size; i++){
+        cout<<a[i]<<" ";
+    }
+
+
+    
+
+    // cout<<a.size();
+}
