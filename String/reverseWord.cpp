d2b9cc57c7f7dbc8aad3c8a78d546dacecf7eb5e
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-14 09:09:21 +0530

    question is not tough but it is not printing something

--- String/reverseWord.cpp
+++ String/reverseWord.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
 using namespace std;
 int main(){
     string str = " the fox make it easy  ";
     string t = "";
     string res = "";
     for(int i=str.length()-1; i>=0; i--){
-        if(str[i]==' ' and !t.empty()){
+        if(str[i]==' ' && !t.empty()){
             continue;
         }
         if(str[i]!=' '){
             t.push_back(str[i]);
         }
-        if(str[i]==' ' and t.empty()){
+        if(str[i]==' ' && t.empty()){
             reverse(t.begin(),t.end());
             res.append(t);
             res.push_back(' ');
@@ -24,5 +25,5 @@ int main(){
         res.pop_back();
     }
 
-    cout<<res;
+    
 }
