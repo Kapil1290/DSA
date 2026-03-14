eca915c83ed40a747cea9b27d887dfe402580d14
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-14 09:12:15 +0530

    not print something yet

--- String/reverseWord.cpp
+++ String/reverseWord.cpp
@@ -4,26 +4,27 @@
 using namespace std;
 int main(){
     string str = " the fox make it easy  ";
-    string t = "";
-    string res = "";
+    string t;
+    string res;
     for(int i=str.length()-1; i>=0; i--){
-        if(str[i]==' ' && !t.empty()){
+        if(str[i]==' ' && !(t.empty())){
             continue;
         }
         if(str[i]!=' '){
             t.push_back(str[i]);
         }
-        if(str[i]==' ' && t.empty()){
+        if(str[i]==' ' && (t.empty())){
             reverse(t.begin(),t.end());
             res.append(t);
-            res.push_back(' ');
+            res.append(" ");
             t.clear();
         }
     }
 
     if(res[res.length()-1] == ' '){
+        cout<<res;
         res.pop_back();
     }
 
-    
+    cout<<res;
 }
