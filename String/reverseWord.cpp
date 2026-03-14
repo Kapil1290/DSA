6eaf45de3a227aaf89bc01ed4ab5cbc7d88cd0ca
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-14 08:58:36 +0530

    problem of the leetcode

--- String/reverseWord.cpp
+++ String/reverseWord.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    string str = " the fox make it easy  ";
+    string t = "";
+    string res = "";
+    for(int i=str.length(); i>=0; i++){
+        if(str[i]==' ' and !t.empty()){
+            
+        }
+    }
 }
