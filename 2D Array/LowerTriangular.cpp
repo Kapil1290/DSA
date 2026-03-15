eac4dff7920876b88e8a2a73fcce1ffbbe49f5da
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-15 10:53:38 +0530

    vector is mistry

--- 2D Array/LowerTriangular.cpp
+++ 2D Array/LowerTriangular.cpp
@@ -10,10 +10,15 @@ int main(){
     
     vector<vector<int>> arr(r, vector<int>(c));
     arr[0].push_back(3);
-    cout<<arr[0][0];
-    cout<<arr[0][1];
+    // cout<<arr[0][0];
+    // cout<<arr[0][1];
     cout<<arr[0][2];
 
 
-    for(int i)
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
