afd680500e2bf2dbc5467ead9e01295e44cfa3cf
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-17 20:20:05 +0530

    problem solving

--- Lecture_5/Trishul.cpp
+++ Lecture_5/Trishul.cpp
@@ -2,3 +2,28 @@
 #include<vector>
 
 using namespace std;
+
+int main(){
+    int n=13;
+    int m=(n+1)/2;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(i<m){
+                if(j%2!=0){
+                   cout<<"*";
+                }else{
+                   cout<<" ";
+                }
+            }else if(i==m){
+                cout<<"*";
+            }else{
+                if(j==(m/2+1)){
+                    cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
