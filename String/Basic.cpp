d923d928f2b883d677ee4fda49d89135c13e6c4d
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-17 20:22:00 +0530

    checking the code

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -1,18 +1,78 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string str;
+//     // cin>>str;
+//     // cout<<str;
+
+//     // cout<<str[1];
+//     // cout<<str.length();
+
+//     // int s = 'R';
+//     // cout<<s;
+
+//     // string str1 = "kapia";
+//     // string str2 = "kapil";
+
+//     // cout<<str1.compare(str2);
+
+
+// }
+
+
+
+
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int main(){
-    string str;
-    // cin>>str;
-    // cout<<str;
+int main() {
+	string s;
+	cin>>s;
+	vector<int> f1(26,0);
+	vector<int> f2(26,0);
 
-    // cout<<str[1];
-    // cout<<str.length();
+	for(int i=0; i<s.size(); i++){
+		if(s[i]>='a' and s[i]<='z'){
+			f1[s[i]-'a']++;
+		}else{
+			f2[s[i]-'A']++;
+		}
+	}
 
-    // int s = 'R';
-    // cout<<s;
+	int val1=0;
+	int idx1;
+	for(int i=0; i<f1.size(); i++){
+		if(val1<f1[i]){
+			val1=f1[i];
+			idx1=i;
+		}
+	}
 
-    string str1 = "kapia";
-    string str2 = "kapil";
+	int val2=0;
+	int idx2;
+	for(int i=0; i<f2.size(); i++){
+		if(val2<f2[i]){
+			val2=f1[i];
+			idx2=i;
 
-    cout<<str1.compare(str2);
+		}
+	}
+
+	if(val1>val2){
+		char c = 'a'-idx1;
+		cout<<c;
+	}else{
+		char c = 'A'-idx2;
+		cout<<c;
+	}
+
+	// if()
+	// sort(f1.begin(),f1.end());
+	// sort(f2.begin(),f2.end());
+	// int m1 = f1[f1.size()-1];
+	// int m2 = f2[f2.size()-1];
+	// cout<<x;
+	// cout<<y;
+	return 0;
 }
