ddc17eba5ba295209cc8b725b5f28f368f247901
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-18 08:22:33 +0530

    problem solving

--- String/Basic.cpp
+++ String/Basic.cpp
@@ -22,55 +22,56 @@
 
 
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-int main() {
-	string s;
-	cin>>s;
-	vector<int> f1(26,0);
-	vector<int> f2(26,0);
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// int main() {
+// 	string s;
+// 	cin>>s;
+// 	vector<int> f1(26,0);
+// 	vector<int> f2(26,0);
 
-	for(int i=0; i<s.size(); i++){
-		if(s[i]>='a' and s[i]<='z'){
-			f1[s[i]-'a']++;
-		}else{
-			f2[s[i]-'A']++;
-		}
-	}
+// 	for(int i=0; i<s.size(); i++){
+// 		if(s[i]>='a' and s[i]<='z'){
+// 			f1[s[i]-'a']++;
+// 		}else{
+// 			f2[s[i]-'A']++;
+// 		}
+// 	}
 
-	int val1=0;
-	int idx1;
-	for(int i=0; i<f1.size(); i++){
-		if(val1<f1[i]){
-			val1=f1[i];
-			idx1=i;
-		}
-	}
-    // cout<<val1<<endl<<idx1<<endl;
+// 	int val1=0;
+// 	int idx1;
+// 	for(int i=0; i<f1.size(); i++){
+// 		if(val1<f1[i]){
+// 			val1=f1[i];
+// 			idx1=i;
+// 		}
+// 	}
+//     // cout<<val1<<endl<<idx1<<endl;
 
-	int val2=0;
-	int idx2;
-	for(int i=0; i<f2.size(); i++){
-		if(val2<f2[i]){
-			val2=f2[i];
-			idx2=i;
+// 	int val2=0;
+// 	int idx2;
+// 	for(int i=0; i<f2.size(); i++){
+// 		if(val2<f2[i]){
+// 			val2=f2[i];
+// 			idx2=i;
 
-		}
-	}
+// 		}
+// 	}
 
 
     
-    // cout<<val2<<endl<<idx2;
+//     // cout<<val2<<endl<<idx2;
 
-	if(val1>val2){
-		char c = 97+idx1;
-		cout<<c;
-	}else{
-		char c =65+idx2;
-		cout<<c;
-	}
+// 	if(val1>val2){
+// 		char c = 97+idx1;
+// 		cout<<c;
+// 	}else{
+// 		char c =65+idx2;
+// 		cout<<c;
+// 	}
 
-	return 0;
-}
+// 	return 0;
+// }
+
