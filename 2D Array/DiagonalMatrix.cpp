3db09b0dfef1b0f957627de983cead2380c717be
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-19 13:46:37 +0530

    problem solving

--- 2D Array/DiagonalMatrix.cpp
+++ 2D Array/DiagonalMatrix.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 int main(){
+
+    // Array creation using simple method 
     int arr[][4] = {
         {1,2,3,4},
         {5,6,7,8},
@@ -10,5 +12,14 @@ int main(){
     int m = sizeof(arr)/(sizeof(int)*sizeof(int));
     int n = sizeof(arr[0])/sizeof(int);
     cout<<m<<" "<<n;
+
+
+    // Vector creation 
+
+    vector<vector<int>> arr = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
 
 }
