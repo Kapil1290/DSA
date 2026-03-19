95cb3f3725fbe9ab57efae1da689826085925ac2
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-19 13:50:10 +0530

    practisek

--- 2D Array/DiagonalMatrix.cpp
+++ 2D Array/DiagonalMatrix.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
 
     // Array creation using simple method 
-    int arr[][4] = {
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11}
-    };
-    int m = sizeof(arr)/(sizeof(int)*sizeof(int));
-    int n = sizeof(arr[0])/sizeof(int);
-    cout<<m<<" "<<n;
+    // int arr[][4] = {
+    //     {1,2,3,4},
+    //     {5,6,7,8},
+    //     {9,10,11}
+    // };
+    // int m = sizeof(arr)/(sizeof(int)*sizeof(int));
+    // int n = sizeof(arr[0])/sizeof(int);
+    // cout<<m<<" "<<n;
 
 
     // Vector creation 
