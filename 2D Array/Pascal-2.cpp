e35143f25e6b7e6d99c80735c5e2470c56f0f21b
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-20 11:30:17 +0530

    pascal problems variants

--- 2D Array/Pascal-2.cpp
+++ 2D Array/Pascal-2.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-// int ncr(int n, int r){
-//     int res=1;
-//     for(int i=0; i<r; i++){
-//         res = res*(n-i);
-//         res = res/(i+1);
+int ncr(int n, int r){
+    int res=1;
+    for(int i=0; i<r; i++){
+        res = res*(n-i);
+        res = res/(i+1);
         
-//     }
-//     return res;
-// }
+    }
+    return res;
+}
 int main(){
     int row = 0;
     if(row==0){
@@ -16,9 +16,9 @@ int main(){
     }
 // 1st approach--- with function
 
-    // for(int i=1; i<=row; i++){
-    //     cout<<ncr(row-1,i-1)<<endl;
-    // }
+    for(int i=1; i<=row; i++){
+        cout<<ncr(row-1,i-1)<<endl;
+    }
 
 
     // 2nd approach---without function
