9cb12e573c63d8239376a18e3ac959cae308e5fc
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-23 09:57:49 +0530

    re write the code again

--- 2D Array/DiagonalTraverse.cpp
+++ 2D Array/DiagonalTraverse.cpp
@@ -22,42 +22,42 @@ int main(){
         {7,8,9,2}
     };
 
-    int row = arr.size();
-    int col = arr[0].size();
+    // int row = arr.size();
+    // int col = arr[0].size();
 
-    int r=0; 
-    int c=0;
+    // int r=0; 
+    // int c=0;
 
-    bool up = true;
+    // bool up = true;
 
-    while(r < row and c < col){
-        if(up==true){
-            while(r>0 and c<col-1){
-                cout<<arr[r][c]<<" ";
-                r--;
-                c++;
-            }
-            cout<<arr[r][c]<<" ";
-            if(c==col-1){
-                r++;
-            }else{
-                c++;
-            }
-        }else{
-            while(r<row-1 and c>0){
-                cout<<arr[r][c]<<" ";
-                r++;
-                c--;
-            }
-            cout<<arr[r][c]<<" ";
-            if(r==row-1){
-                c++;
-            }else{
-                r++;
-            }
-        }
-        up=!up;
-    }
+    // while(r < row and c < col){
+    //     if(up==true){
+    //         while(r>0 and c<col-1){
+    //             cout<<arr[r][c]<<" ";
+    //             r--;
+    //             c++;
+    //         }
+    //         cout<<arr[r][c]<<" ";
+    //         if(c==col-1){
+    //             r++;
+    //         }else{
+    //             c++;
+    //         }
+    //     }else{
+    //         while(r<row-1 and c>0){
+    //             cout<<arr[r][c]<<" ";
+    //             r++;
+    //             c--;
+    //         }
+    //         cout<<arr[r][c]<<" ";
+    //         if(r==row-1){
+    //             c++;
+    //         }else{
+    //             r++;
+    //         }
+    //     }
+    //     up=!up;
+    // }
 
 
 }
