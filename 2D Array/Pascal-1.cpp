7dac307a0f4a367703a724f62562cbbf148d4395
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-24 08:06:06 +0530

    re write the code

--- 2D Array/Pascal-1.cpp
+++ 2D Array/Pascal-1.cpp
@@ -15,38 +15,39 @@
 // }
 
 
-#include<iostream>
-#include<vector>
-using namespace std;
-int main(){
-    int r;
-    cin>>r;
-    vector<vector<int>> a;
-    if(r==0){
-        return 0;
-    }
-    a.push_back(vector<int>(1,1));
-    cout<<1;
-    cout<<endl;
-    for(int i=1; i<r; i++){
-        vector<int> cur;
-        cur.push_back(1);
-        cout<<1<<" ";
-        for(int j=1; j<i; j++){
-            int val = a[i-1][j-1]+ a[i-1][j];
-            cur.push_back(val);
-            cout<<val<<" ";
-        }
-        cur.push_back(1);
-        cout<<1<<" ";
-        cout<<endl;
-        a.push_back(cur);
-    }
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     int r;
+//     cin>>r;
+//     vector<vector<int>> a;
+//     if(r==0){
+//         return 0;
+//     }
+//     a.push_back(vector<int>(1,1));
+//     cout<<1;
+//     cout<<endl;
+//     for(int i=1; i<r; i++){
+//         vector<int> cur;
+//         cur.push_back(1);
+//         cout<<1<<" ";
+//         for(int j=1; j<i; j++){
+//             int val = a[i-1][j-1]+ a[i-1][j];
+//             cur.push_back(val);
+//             cout<<val<<" ";
+//         }
+//         cur.push_back(1);
+//         cout<<1<<" ";
+//         cout<<endl;
+//         a.push_back(cur);
+//     }
 
-    // for(int i=0; i<r; i++){
-    //     for(int j=0; j<r; j++){
-    //         cout<<a[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-}
+//     // for(int i=0; i<r; i++){
+//     //     for(int j=0; j<r; j++){
+//     //         cout<<a[i][j]<<" ";
+//     //     }
+//     //     cout<<endl;
+//     // }
+// }
+
