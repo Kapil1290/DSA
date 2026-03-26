d31a48e7da62f6b1cb168b6e44696d02d0526510
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-26 15:54:19 +0530

    creating array

--- Lecture_8/AnkitAndBhanu.cpp
+++ Lecture_8/AnkitAndBhanu.cpp
@@ -2,5 +2,10 @@
 #include<vector>
 using namespace std;
 int main(){
-    
+    vector<int> a;
+    a.push_back(10);
+    a.push_back(2);
+    a.push_back(8);
+    a.push_back(6);
+    a.push_back(9);
 }
