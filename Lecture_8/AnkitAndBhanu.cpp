68e761edb826e30f6dd7d24c3789dfae96c65c7f
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-27 12:08:24 +0530

    checking the code

--- Lecture_8/AnkitAndBhanu.cpp
+++ Lecture_8/AnkitAndBhanu.cpp
@@ -4,12 +4,7 @@ using namespace std;
 int main(){
     vector<int> a;
     a.push_back(10);
-    a.push_back(2);
-    a.push_back(8);
-    a.push_back(6);
-    a.push_back(9);
-    a.push_back(5);
-    a.push_back(1);
+    
 
     int l=0; int h=a.size()-1;
     int Ascore = 0;
