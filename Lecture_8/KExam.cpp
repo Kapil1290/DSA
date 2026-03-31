792f85568abe97322fd873bef5b764fa1ba8ac07
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-31 15:23:21 +0530

    problem solving

---
+++ Lecture_8/KExam.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> arr = {1,2,3,4,5,6,7,8};
+    int k=3;
+    int a=0;
+    for(int i=0; i<arr.size()/k; i++){
+        reverse(arr.begin()+a, arr.begin()+)
+    }
+}
