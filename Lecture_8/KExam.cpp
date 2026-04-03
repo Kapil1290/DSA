657afd712863df7fcb8e955b525f286a240d66d6
Kapil1290 <kapilprajapat9303@gmail.com>
2026-04-03 10:38:21 +0530

    array changing

--- Lecture_8/KExam.cpp
+++ Lecture_8/KExam.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    vector<int> arr = {1,2,3,4,5,6,7,8};
+    vector<int> arr = {1,2,3,5,5,6,7,8};
     int k=3;
     int a=0;
     for(int i=0; i<arr.size()/k; i++){
