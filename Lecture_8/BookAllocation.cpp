10d7803df7bd90b8ec701397107569e735584dc4
Kapil1290 <kapilprajapat9303@gmail.com>
2026-04-08 13:43:35 +0530

    new tough problem

--- Lecture_8/BookAllocation.cpp
+++ Lecture_8/BookAllocation.cpp
@@ -9,8 +9,8 @@ bool isValid(){
 
 int main(){
     vector<int> arr = {2,3,5,7,8};
-    int l;
-    int h;
+    int l=0;
+    int h=0;
     // for(int i: arr){
     //     l = max(l, i);
     //     h += i;
