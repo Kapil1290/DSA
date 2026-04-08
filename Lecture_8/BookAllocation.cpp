187d531a1d3839c0ac14dc6c201c634efa966d6a
Kapil1290 <kapilprajapat9303@gmail.com>
2026-04-08 13:44:24 +0530

    check the for each loop

--- Lecture_8/BookAllocation.cpp
+++ Lecture_8/BookAllocation.cpp
@@ -11,13 +11,10 @@ int main(){
     vector<int> arr = {2,3,5,7,8};
     int l=0;
     int h=0;
-    // for(int i: arr){
-    //     l = max(l, i);
-    //     h += i;
-    // }
-    for(int i=0; i<arr.size(); i++){
-        l = max(l, arr[i]);
-        h += arr[i];
+    for(int i: arr){
+        l = max(l, i);
+        h += i;
     }
+
     cout<<l<<" "<<h;
 }
