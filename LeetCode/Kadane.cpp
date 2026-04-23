58466c6e8ae219d298c1dba5ae472c65c3b40bfd
Kapil1290 <kapilprajapat9303@gmail.com>
2026-04-23 14:14:53 +0530

    kadane algorithm

--- LeetCode/Kadane.cpp
+++ LeetCode/Kadane.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-in
+int main(){
+    cout<<"Enter limit of arrayy:-";
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    
+}
