f16bbb6a35679e06ce77aa22499533b321f57938
Kapil <kapilprajapat9303@gmail.com>
2026-06-15 14:26:00 +0530

    new task basic operation based hospital mgt system

--- SIP/Lec10/hospital.js
+++ SIP/Lec10/hospital.js
@@ -1,1 +1,26 @@
 
+function registerPatientDetails(name, contact, age, gender, room){
+    name = name.toUpperCase();
+    contact=contact;
+    age = age;
+    gender = gender.toUpperCase();
+    let ID = floor(Math.random()*1000000);
+    console.log(ID)
+    let check = 1;
+
+    console.log("----------Patient Successfully Registered-----------")
+
+    return{
+        getRoomOfPatient(){
+            return room;
+        },
+        getPatientInfo(){
+            
+        }
+        
+    }
+    
+}
+
+let register = registerPatientDetails("kapil", "58469865", 23, "male", "01")
+console.log(register)
