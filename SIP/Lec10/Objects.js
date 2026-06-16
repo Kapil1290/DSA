f5e60f95940b701aa5e9fb9fea7a6ea0d583d0dc
Kapil <kapilprajapat9303@gmail.com>
2026-06-16 14:31:23 +0530

    object in js

---
+++ SIP/Lec10/Objects.js
@@ -0,0 +1,21 @@
+
+let detail = {
+    name: "kapil",
+    age: 21,
+    contact: 232324,
+    city: "indore",
+    address:{
+        place: "Neww York city", 
+        pin: 452022
+    }
+}
+
+// delete detail.name;
+console.log(detail)
+
+// loop on object 
+for( key in detail){
+    console.log(key, detail[key]) //
+}
+
+let arr = [a={name:"kapil"}, ]
